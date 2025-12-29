9d5049c36abda3caa8686a2464b7debcdb899bed
hello.maxsukach <hello.maxsukach@gmail.com>
2025-12-29 12:08:59 +0000

    Changes before Firebase Studio auto-run

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+      "9000-firebase-warlordsnextjs-1766877686696.cluster-zti5ytzhlffjjqj6bp4giuli3u.cloudworkstations.dev",
+      "9002-firebase-warlordsnextjs-1766877686696.cluster-zti5ytzhlffjjqj6bp4giuli3u.cloudworkstations.dev"
+    ],
+  },
 };
 
 export default nextConfig;
